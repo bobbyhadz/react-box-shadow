9c322ab5b4297b2692d413da0d9d71bd37d02f93
bobbyhadz <opmftw@gmail.com>
2024-04-07 14:40:37 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  const divStyles = {
+    boxShadow: '1px 2px 9px #F4AAB9',
+    margin: '4em',
+    padding: '1em',
+  };
+
+  return (
+    <div>
+      <div style={divStyles}>Hello world</div>
+    </div>
+  );
+};
+
+export default App;
